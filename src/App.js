56df50a778a0aa3e843dea36953912063a708769
Bondok6 <kirolousbondok6@gmail.com>
2022-03-07 22:16:13 +0200

    Add components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage';
+import DetailsPage from './pages/DetailsPage';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/details" element={<DetailsPage />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/countries/countries.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const countries = () => {
+  return <div>countries</div>;
+};
+
+export default countries;
