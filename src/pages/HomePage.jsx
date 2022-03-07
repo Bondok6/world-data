ebeff3f3a3d99ed17a4a890db24294862f7fd8f4
Bondok6 <kirolousbondok6@gmail.com>
2022-03-07 22:15:58 +0200

    Add two pages (Home, Details)

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Continents from '../components/continents/Continents';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Continents />
+    </div>
+  );
+};
+
+export default HomePage;
